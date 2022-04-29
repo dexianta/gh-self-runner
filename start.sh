a88ec6bd2bc3973f6d90fecf5f99068f8103b1f2
dexian <dexian@benshi.ai>
2022-04-29 13:22:58 +0200

    update

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ echo "2b97bd3f4639a5df6223d7ce728a611a4cbddea9622c1837967c83c86ebb2baa  actions-
 tar xzf ./actions-runner-linux-x64-2.290.1.tar.gz
 
 # more steps need to be taken here
-./config.sh --url https://github.com/benshi-ai --token $2 --work _work --name ${PWD##*/} --labels self-hosted --runnergroup default --replace
+./config.sh --url https://github.com/benshi-ai --token $token --work _work --name $name --labels self-hosted --runnergroup default --replace
 
 ./run.sh
 
