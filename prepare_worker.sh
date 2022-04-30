fbfe16b28f4795aaedcb0ecb28ea2f760f036ca2
benshi.runner <benshi.runner1@gmail.com>
2022-04-30 13:18:16 +0200

    update

--- prepare_worker.sh
+++ prepare_worker.sh
@@ -16,4 +16,4 @@ cd $path
 curl -o actions-runner-linux-x64-2.290.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.290.1/actions-runner-linux-x64-2.290.1.tar.gz
 echo "2b97bd3f4639a5df6223d7ce728a611a4cbddea9622c1837967c83c86ebb2baa  actions-runner-linux-x64-2.290.1.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.290.1.tar.gz
-./config.sh --url https://github.com/benshi-ai --token AN5B6KN35ZRGLI5XBWOA7V3CNUKES --name runner-"$runner_suffix" --labels self-hosted --runnergroup default --replace
+./config.sh --url https://github.com/benshi-ai --token $token --name runner-"$runner_suffix" --labels self-hosted --runnergroup default --replace
